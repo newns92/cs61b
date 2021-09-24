28fe534bccf7829fa2fe00a4b8a3f32075d625bf
Newns <s.newns92@gmail.com>
2021-09-24 12:23:33 -0400

    reading 1

---
+++ HelloNumbers.java
@@ -0,0 +1,21 @@
+public class HelloNumbers {
+    public static void main(String[] args) {
+        int x = 0; // x must be declared before it is used, + it must be given a type (type of a variable can never change (STATIC))
+        while (x < 10) {
+            System.out.print(x + " ");
+            x++;
+        }
+
+        System.out.println();
+        System.out.println(5 + "10");
+
+        int sum = 0;
+        x = 0; // reset counter
+        // print out cumulative sum
+        while (x < 10) {
+            sum += x;
+            System.out.print(sum + "\n");
+            x++;
+        }
+    }
+}
